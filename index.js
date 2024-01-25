ebd552edd9a4f48a0b27d588ae9f7ae0fde485ec
clasificacionEcografias <157669527+clasificacionEcografias@users.noreply.github.com>
2024-01-25 00:54:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function verificarValor() {
     // Verificar si el valor ingresado es igual al valor preestablecido
     if (valorIngresado === valorPreestablecido) {
         // Obtener la URL base del repositorio en GitHub
-        var githubURL = 'https://clasificacionecografias.github.io/Patologias-Renales'; // Reemplaza con tu nombre de usuario y nombre de repositorio
+        var githubURL = 'https://clasificacionecografias.github.io/Patologias-Renales/'; // Reemplaza con tu nombre de usuario y nombre de repositorio
         // Redirigir a otra página web en GitHub
-        window.location.href = githubURL + '/evalue.html';
+        window.location.href = githubURL + 'evalue.html';
     } else {
         // Mostrar un mensaje si el valor no coincide
         alert('El valor ingresado no es válido. Inténtelo de nuevo.');
